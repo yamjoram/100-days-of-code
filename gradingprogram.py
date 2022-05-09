e354031719e827cef434fce059bf28dcac18cb03
yamjoram <99007890+yamjoram@users.noreply.github.com>
2022-05-10 00:23:22 +0530

    editing of dictionary

---
+++ gradingprogram.py
@@ -0,0 +1,24 @@
+student_scores = {
+    "Harry" : 81,
+    "Ron" : 78,
+    "Hermione" : 99,
+    "Draco" : 74,
+    "Neville" : 62,
+    }
+
+student_grades = {}
+
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "outstanding"
+    elif score > 80:
+        student_grades[student] = "exceeds expectation"
+    elif score > 70:
+        student_grades[student] = "acceptable"
+    else: 
+        student_grades[student] = "fail"
+
+
+
+print(student_grades)
