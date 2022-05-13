d1a11be6b5e2e01098396e39faf12afbc1d42047
yamjoram <99007890+yamjoram@users.noreply.github.com>
2022-05-13 23:14:20 +0530

    function with output

---
+++ function&output1.py
@@ -0,0 +1,9 @@
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    foramted_l_name = l_name.title()
+    return f"{formated_f_name}  {foramted_l_name}"
+
+print(format_name("joram", "Joram"))
+
+
+    
