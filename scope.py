a6651b9f0789ff77803abcb49b60f33e6915a117
yamjoram <99007890+yamjoram@users.noreply.github.com>
2022-05-23 23:33:54 +0530

    learn the meaning of scope

---
+++ scope.py
@@ -0,0 +1,13 @@
+enemies = 1
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+
+print(f"enemies oustide function: {enemies}")
+
+
+
+
