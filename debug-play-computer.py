c9a374bd28ac4c74f85a1b8358e97ee38fa329e8
yamjoram <99007890+yamjoram@users.noreply.github.com>
2022-05-25 23:13:24 +0530

    debug and play with computer

---
+++ debug-play-computer.py
@@ -0,0 +1,9 @@
+
+
+# play computer
+
+year = int(input("what's your year of birth?"))
+if year > 1980 and year < 1994:
+    print("you are a millenial.")
+elif year >= 1994:
+    print("you are a gen z.")
