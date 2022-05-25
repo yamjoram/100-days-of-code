eeb5b32397d40719f1dcbe9c7eb9467498a5a207
yamjoram <99007890+yamjoram@users.noreply.github.com>
2022-05-25 23:46:54 +0530

    debug is important, for what it's each line mean

---
+++ debug6.py
@@ -0,0 +1,23 @@
+
+#use a debugger
+
+# def mutate(a_list):
+#     b_list = []
+#     for item in a_list:
+#         new_item = item * 2
+#     b_list.append(new_item)
+#     print(b_list)
+
+# mutate([1,2,3,5,8,13])
+
+#fixing the code
+
+
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+
+mutate([1,2,3,5,8,13])
