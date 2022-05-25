9dec5cadfade409e79693bd90bbd9e26a9375e8c
yamjoram <99007890+yamjoram@users.noreply.github.com>
2022-05-26 00:13:24 +0530

    how to debug FizzBuzz

---
+++ debugging-fizzbug.py
@@ -0,0 +1,25 @@
+
+
+
+# for number in range(1, 101):
+#     if number % 3 == 0 or number % 5 == 0:
+#         print("FizzBuzz")
+#     if number % 3 == 0:
+#         print("Fizz")
+#     if number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print([number])
+
+# fixing
+
+for number in range(1, 101):
+    print(f"currently on number {number}")
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
