321079a1966499a6b3352f1af2db0565e74ac984
yamjoram <99007890+yamjoram@users.noreply.github.com>
2022-05-25 23:22:32 +0530

    how to fix errors

---
+++ debug4.py
@@ -0,0 +1,12 @@
+
+
+# fix the errors
+# age = input("How old are you?")
+# if age > 18:
+#     print("you can drive at age {age}.")
+
+# fixing
+
+age = int(input("How old are you?"))
+if age > 18:
+    print(f"you can drive at age {age}.")
